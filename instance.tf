65f3271b47dca44968b6929f4201e8063b758e0d
prathjan <prathjan@cisco.com>
2021-11-11 08:14:21 -0800

    private key base64

--- instance.tf
+++ instance.tf
@@ -62,6 +62,7 @@ resource "null_resource" "vm_node_init" {
 
 locals {
   host = data.terraform_remote_state.host.outputs.host
-  privatekey = data.terraform_remote_state.global.outputs.privatekey
+  #privatekey = data.terraform_remote_state.global.outputs.privatekey
+  privatekey = base64decode(data.terraform_remote_state.global.outputs.privatekey)
 }
 
